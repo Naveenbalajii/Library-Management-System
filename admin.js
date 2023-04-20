48fe46b06447ac77a02571e099a9801bf4f54cfe
Naveen Balaji <88662627+Naveenbalajii@users.noreply.github.com>
2023-04-20 10:34:32 +0530

    Update admin.js

--- admin.js
+++ admin.js
@@ -1,12 +1,11 @@
-const firebaseConfig = {
-    apiKey: "AIzaSyDiroSo8ssUYhbuMNb8-WN9M9Ysx6i6kU4",
-    authDomain: "library-management-syste-88962.firebaseapp.com",
-    projectId: "library-management-syste-88962",
-    storageBucket: "library-management-syste-88962.appspot.com",
-    messagingSenderId: "694495006577",
-    appId: "1:694495006577:web:d4bcd461e22189b907f0d0",
-    measurementId: "G-0JK3RZP7LL",
-    databaseURL: "https://library-management-syste-88962-default-rtdb.firebaseio.com"
+var firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_AUTH_DOMAIN",
+  databaseURL: "YOUR_DATABASE_URL",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_STORAGE_BUCKET",
+  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+  appId: "YOUR_APP_ID"
 };
 
 firebase.initializeApp(firebaseConfig);
